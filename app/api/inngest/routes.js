bcf52f450ac33cee7d7d3d74edb06fe7e1a7faec
abdalmajeed77 <abdalmajeedmutaher@gmail.com>
2025-02-19 21:00:05 +0530

    Update routes.js

--- app/api/inngest/routes.js
+++ app/api/inngest/routes.js
@@ -12,11 +12,3 @@ export const { GET, POST, PUT } = serve({
     syncUserUpdate
   ],
 });
-export const helloWorld = inngest.createFunction(
-    { id: "hello-world" },
-    { event: "test/hello.world" },
-    async ({ event, step }) => {
-      await step.sleep("wait-a-moment", "1s");
-      return { message: `Hello ${event.data.email}!` };
-    },
-  );
